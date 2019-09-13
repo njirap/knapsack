5b67ee866b5c934c86cd2269bf47a942ea562598
njirap <njirap@gmail.com>
2019-09-13 17:04:48 +0300

    main: add main function

--- main.py
+++ main.py
@@ -3,10 +3,37 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-def calculate(total, trucks):
-    response = "{}{}".format(total,trucks)
-    return response
-
+def calculate(tonnes, ratios, index):
+    ratios[index][1] = ratios[index][1] + tonnes // ratios[index][0]
+    remainder = tonnes % ratios[index][0]
+    if remainder == 0:
+        return ratios
+    else:
+        for item in ratios[:index][::-1]:
+            if remainder % item[0] == 0:
+                item[1] = remainder // item[0]
+                return ratios
+        for item in ratios[:index][::-1]:
+            if remainder // item[0] > 0:
+                item[1] = remainder // item[0]
+                remainder = remainder % item[0]
+                for item2 in ratios[:index-1][::-1]:
+                    if remainder % item2[0] == 0:
+                        item2[1] = remainder // item2[0]
+                        return ratios
+                if len(ratios[:index-1]) == 0:
+                    response = "Perfect distribution not possible\n"
+                    response = response + "Ratios: {}, Uncarried balance: {}".format(ratios, remainder)
+                    return response
+                if ratios[index][1] < 2:
+                    remainder = remainder + ratios[index+1][0]
+                    ratios[index+1][1] -= 1
+                else:
+                    remainder = remainder + ratios[index][0]
+                    ratios[index][1] -= 1
+                    index -= 1
+                calculate(remainder, ratios, index)
+                return ratios
 
 @app.route('/')
 def get_ratio():
